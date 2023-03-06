942137739e3b75ace3934ed40df95e7c8bf5b664
Kravchene <kravchene@mail.ru>
2023-03-06 19:04:53 +0300

    Fix

--- src/by/itacademy/courses/hw9/Main.java
+++ src/by/itacademy/courses/hw9/Main.java
@@ -1,2 +1,30 @@
-package by.itacademy.courses.hw9;public class Main {
+package by.itacademy.courses.hw9;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String number = in.next();
+        String symbol=null;
+        Pattern pattern = Pattern.compile("[-+/*]");
+        Matcher matcher = pattern.matcher(number);
+        while (matcher.find()) {symbol=  number.substring(matcher.start(),matcher.end());}
+        String[] words = number.split("\\+|-|/|\\*");
+        double a = Double.parseDouble(words[0]);
+        double b = Double.parseDouble(words[1]);
+        Calculator<Double, Double,String> calculator = new Calculator<>(a,b,symbol);
+        calculator.sad();
+        System.out.println();
+
+        Pair<String> pair=new Pair<>("Инь","Ян");
+        System.out.println(pair.getYan());
+        System.out.println(pair.getYin());
+        pair.setYan("Свет");pair.setYin("Тьма");
+        System.out.println(pair.toString());
+        pair.setYin(pair.yan);pair.setYan(pair.yin);
+        System.out.println(pair.toString());
+    }
 }
--- src/by/itacademy/courses/hw9/Pair.java
+++ src/by/itacademy/courses/hw9/Pair.java
@@ -1,2 +1,31 @@
-package by.itacademy.courses.hw9;public class Pair {
+package by.itacademy.courses.hw9;
+
+public class Pair<T> {
+    T yin;
+    T yan;
+
+    Pair(T yin,T yan){
+        this.yan=yan;
+        this.yin=yin;
+    }
+
+    public T getYan() {
+        return yan;
+    }
+
+    public void setYan(T yan) {
+        this.yan = yan;
+    }
+    public T getYin() {
+        return yin;
+    }
+    public void setYin(T yin) {
+        this.yin = yin;
+    }
+
+    @Override
+    public String toString() {
+        return "yin='" + yin + '\'' +
+                ", yan='" + yan;
+    }
 }
